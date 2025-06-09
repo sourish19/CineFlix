164c6183cea01b779fdf3ada80aec8faf5a980bd
Sourish Dey <sourish19z@gmail.com>
2025-06-10 04:01:52 +0530

    Added Delete Movie Card & Updated create movie

--- src/main.js
+++ src/main.js
@@ -32,7 +32,7 @@ const renderMovies = (
             <div
               class="movie-details-container flex gap-1.5 flex-col border-y-2 my-2"
             >
-              <div class="movie-name">${formatMovieTitle(movieTitle)}</div>
+              <div class="bb movie-name">${formatMovieTitle(movieTitle)}</div>
               <div class="movie-desc">${movieDesc}</div>
               <div class="movie-meta movie-genre">
                 Genre:
@@ -54,8 +54,8 @@ const renderMovies = (
               </div>
             </div>
             <div class="flex justify-end gap-2 items-center">
-              <button class="bttn">Details</button>
-              <button class="bttn">Delete</button>
+              <button class="details-bttn bttn">Details</button>
+              <button class="dele-bttn bttn">Delete</button>
             </div>`;
   movieCardContainer.append(div);
 };
@@ -117,16 +117,16 @@ const defaultImage = (image) => {
 };
 
 // Remove Modal Container
-const removeModalContainer = () => {
-  modalContainer.classList.toggle("hidden", true);
+const removeModalContainer = (container) => {
+  container.classList.toggle("hidden", true);
   mainContainer.classList.toggle("opacity-10", false);
   mainContainer.classList.toggle("backdrop-blur", false);
   mainContainer.classList.toggle("bg-black/50", false);
 };
 
 // Show Modal Container
-const showModalContainer = () => {
-  modalContainer.classList.toggle("hidden", false);
+const showModalContainer = (container) => {
+  container.classList.toggle("hidden", false);
   mainContainer.classList.toggle("opacity-10", true);
   mainContainer.classList.toggle("backdrop-blur", true);
   mainContainer.classList.toggle("bg-black/50", true);
@@ -134,24 +134,23 @@ const showModalContainer = () => {
 
 // Remove Modal Container Event
 crossBttn.addEventListener("click", () => {
-  removeModalContainer();
+  removeModalContainer(modalContainer);
 });
 
 // Show Modal Container Event
 createMovieBttn.addEventListener("click", (e) => {
-  showModalContainer();
+  showModalContainer(modalContainer);
 });
 
+// Get Modal Movie Genre
 const getMovieGenre = () => {
-  const arrayGenre = genre.map((val) => {
-    const radios = document.getElementsByName(val);
-    if (radios[0].checked) {
-      return radios[0].value;
+  const arrayGenre = genre.filter((val) => {
+    const element = document.getElementsByName(val);
+    if (element[0].checked) {
+      return element[0].value;
     }
   });
-  console.log(String(arrayGenre));
-
-  // return arrayGenre;
+  return String(arrayGenre);
 };
 
 // Modal Container inputs
@@ -166,7 +165,7 @@ movieCreateBttn.addEventListener("click", () => {
   } else {
     const createModalMovieName = movieCreateName.value;
     const createModalMovieDesc = movieCreateDesc.value;
-    const createModalMovieRating = movieCreateRating.value;
+    const createModalMovieRating = Number(movieCreateRating.value);
     const createModalMovieYear = movieCreateYear.value;
     const createModalMovieGenre = getMovieGenre();
     renderMovies(
@@ -177,12 +176,107 @@ movieCreateBttn.addEventListener("click", () => {
       createModalMovieRating,
       createModalMovieDesc
     );
-
-    removeModalContainer();
+    removeModalContainer(modalContainer);
   }
 });
-// ratingsBttn.addEventListener("click", (e) => {
-//   console.log("jjj");
-// });
 
+// Global Event Listner
+const globalEventListner = (eventType, selector, callback) => {
+  document.addEventListener(eventType, (event) => {
+    console.log(event.target);
+
+    if (event.target.matches(selector)) {
+      callback(event);
+    }
+  });
+};
+
+// Delete Movie Card
+globalEventListner("click", ".dele-bttn", (e) => {
+  const confirmDelete = confirm("Do you want to delete this movie");
+  if (confirmDelete === true) {
+    const parentDivCard = e.target.closest(".movie-card");
+    parentDivCard.remove();
+  }
+});
+
+// Details Movie Card
+const detailsModalMovieCard = () => {
+  const div = document.createElement("div");
+  div.innerHTML = `<div
+      class="details-modal-container fixed transform -translate-x-1/2 -translate-y-1/2 top-[50%] left-[50%] bg-gray-500 text-white w-5/12 h-[78%] py-4 px-5"
+    >
+      <div class="px-3 py-3 flex flex-col min-h-[460px]">
+        <div class="details-cross-bttn text-right">
+          <i class="fa-solid fa-xmark"></i>
+        </div>
+        <img
+          src="./interstellar.jpeg"
+          alt=""
+          class="movie-img h-50 object-cover"
+        />
+        <div
+          class="movie-details-container flex gap-1.5 flex-col border-y-2 my-2"
+        >
+          <div class="details-movie-name movie-name">Interstellar</div>
+          <div class="details-movie-desc text-sm text-gray-800">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus
+            quos officiis cupiditate sunt dolorem velit id laboriosam ratione
+            saepe voluptas. Lorem, ipsum dolor sit amet consectetur adipisicing
+            elit. Voluptate sapiente provident optio magni dolores. Nemo facere,
+            quo mollitia aliquam, harum iure minus tempora praesentium corporis
+            iste aperiam architecto aspernatur dolorum!
+          </div>
+          <div class="movie-meta movie-genre">
+            Genre:
+            <span class="details-movie-meta-genre movie-meta-content"
+              >action,comedy</span
+            >
+          </div>
+          <div class="movie-meta">
+            Release Year:
+            <span class="details-movie-meta-year movie-meta-content">2003</span>
+          </div>
+          <div class="movie-meta">
+            Rating:
+            <span class="details-movie-meta-rating movie-meta-content">2</span>
+          </div>
+        </div>
+      </div>
+    </div>`;
+  mainContainer.append(div);
+};
+
+// Details Movie Card Event
+globalEventListner("click", ".details-bttn", (e) => {
+  const parentDiv = e.target.closest(".movie-card");
+  const parentDivMovieImage = parentDiv.querySelector(".movi-img").src;
+  const parentDivMovieName = parentDiv.querySelector(".movie-name").innerText;
+  const parentDivMovieDesc = parentDiv.querySelector(".movie-desc").innerText;
+  const parentDivMovieGenre = parentDiv.querySelector(
+    ".movie-genre-content"
+  ).innerText;
+  const parentDivMovieYear = parentDiv.querySelector(
+    ".movie-release-content"
+  ).innerText;
+  const parentDivMovieRating = parentDiv.querySelector(
+    ".movie-rating-content"
+  ).innerHTML;
+  // console.log(
+  //   parentDivMovieImage,
+  //   parentDivMovieName,
+  //   parentDivMovieDesc,
+  //   parentDivMovieGenre,
+  //   parentDivMovieYear,
+  //   parentDivMovieRating
+  // );
+  detailsModalMovieCard();
+});
+
+globalEventListner("click", ".details-cross-bttn", (e) => {
+  if (e.target.matches(".details.cross-bttn")) {
+    console.log("hii");
+  }
+});
+// Render Movies from API function call
 getMoviesFromApi();
